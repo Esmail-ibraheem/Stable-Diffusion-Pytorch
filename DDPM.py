a694dbdfca6bedc7ac14474bceefc34eb6b8caa6
Esmail Atta <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-04-14 08:56:37 +0300

    Update DDPM.py

--- DDPM.py
+++ DDPM.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np    
    
 class GaussingDistribution:     
-    def __init__(self, parameters: torch.Tensor) -> None:  
+    def __init__(self, parameters: torch.Tensor) -> None:   
         self.mean, log_variance = torch.chunk(parameters, 2, dim=1)
         self.log_variance = torch.clamp(log_variance, -30.0, 20.0)
         self.std = torch.exp(0.5 * self.log_variance)
