78a0230a0b64fce02cdfdd0a26ccecd400c66ec7
Esmail <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-08-28 14:48:37 +0300

    stable diffusion using gradio

--- ddpm.py
+++ ddpm.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
 
-class DDPMSampler:
+class DDPMSampler: # Denoising Diffusion Probabilistic Models Sampler 
 
     def __init__(self, generator: torch.Generator, num_training_steps=1000, beta_start: float = 0.00085, beta_end: float = 0.0120):
         # Params "beta_start" and "beta_end" taken from: https://github.com/CompVis/stable-diffusion/blob/21f890f9da3cfbeaba8e2ac3c425ee9e998d5229/configs/stable-diffusion/v1-inference.yaml#L5C8-L5C8
--- sd_inference.py
+++ sd_inference.py
@@ -1,5 +1,6 @@
 from nanograd.models.stable_diffusion import model_loader
 from nanograd.models.stable_diffusion import pipeline
+
 from PIL import Image
 from pathlib import Path
 from transformers import CLIPTokenizer
@@ -7,14 +8,14 @@ import torch
 
 DEVICE = "cpu"
 
-ALLOW_CUDA = False
+ALLOW_CUDA = False 
 ALLOW_MPS = False
 
 if torch.cuda.is_available() and ALLOW_CUDA:
     DEVICE = "cuda"
 elif (torch.has_mps or torch.backends.mps.is_available()) and ALLOW_MPS:
     DEVICE = "mps"
-print(f"Using device: {DEVICE}")
+print(f"Using device: {DEVICE}")    
 
 tokenizer = CLIPTokenizer("nanograd\models\stable_diffusion\sd_data\\tokenizer_vocab.json", merges_file="nanograd\models\stable_diffusion\sd_data\\tokenizer_merges.txt")
 model_file = "nanograd\models\stable_diffusion\sd_data\\v1-5-pruned-emaonly.ckpt"
@@ -62,7 +63,7 @@ def run():
     )
 
     output_image = Image.fromarray(output_image)
-    output_path = "nanograd\models\stable_diffusion\output\\test.png"
+    output_path = "nanograd\models\stable_diffusion\output\\c.png"
     output_image.save(output_path)
     print(f"Image saved as {output_path}")
 
