4e8765e80bc24c46f9f8c8a77e7a9eba94b1318a
Esmail <113830751+Esmail-ibraheem@users.noreply.github.com>
2024-08-29 08:55:50 +0300

    Update decoder.py

--- decoder.py
+++ decoder.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
-from nanograd.models.stable_diffusion.attention import SelfAttention
+from attention import SelfAttention
 
 class VAE_AttentionBlock(nn.Module):
     def __init__(self, channels):
